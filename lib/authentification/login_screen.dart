2ea3dfaa7de8a4397eef442194dd3c3537b355ef
MurguAdrian <murgu.adrian95@gmail.com>
2024-01-31 15:22:30 +0200

    add_Login

--- lib/authentification/login_screen.dart
+++ lib/authentification/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -11,6 +12,21 @@ class LoginScreen extends StatefulWidget {
 String adminEmail = '';
 String adminPass = '';
 
+allowAdminToLogin() async {
+  User? currentAdmin;
+  await FirebaseAuth.instance.signInWithEmailAndPassword(
+    email: adminEmail, password: adminPass,).then((fAuth) {
+    currentAdmin = fAuth.user;
+  }).catchError((onError) {
+    final snackbar = SnackBar(
+      content: Text(
+        "Error Occured :" + onError.toString(),
+        style: const TextStyle(fontSize: 36,),
+      ),backgroundColor: Colors.blueAccent,
+    );
+  });
+      }
+
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
@@ -20,7 +36,10 @@ class _LoginScreenState extends State<LoginScreen> {
         children: [
           Center(
             child: Container(
-              width: MediaQuery.of(context).size.width * .5,
+              width: MediaQuery
+                  .of(context)
+                  .size
+                  .width * .5,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -34,18 +53,62 @@ class _LoginScreenState extends State<LoginScreen> {
                     },
                     style: TextStyle(fontSize: 16, color: Colors.black),
                     decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.greenAccent,
-                          width: 2,
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.greenAccent,
+                            width: 2,
+                          ),
                         ),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.blueAccent,
+                            width: 2,
+                          ),
+                        ),
+                        hintText: "email",
+                        hintStyle: TextStyle(color: Colors.black12),
+                        icon: Icon(
+                          Icons.email_outlined,
                           color: Colors.blueAccent,
-                          width: 2,
+                        )),
+                  ),
+                  const SizedBox(height: 10),
+                  //pass textfield
+                  TextField(
+                    onChanged: (value) {
+                      adminPass = value;
+                    },
+                    obscureText: true,
+                    style: TextStyle(fontSize: 16, color: Colors.black),
+                    decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.greenAccent,
+                            width: 2,
+                          ),
                         ),
-                      ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.blueAccent,
+                            width: 2,
+                          ),
+                        ),
+                        hintText: "Password",
+                        hintStyle: TextStyle(color: Colors.black12),
+                        icon: Icon(
+                          Icons.lock,
+                          color: Colors.blueAccent,
+                        )),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+
+                    child: const Text(
+                      "Login",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          letterSpacing: 2.5),
                     ),
                   )
                 ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:rge_1/Screen/home_page.dart';
 import 'package:rge_1/authentification/login_screen.dart';
 
 import 'Screen/login_page.dart';
 
-void main()  {
+Future<void> main() async {
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
